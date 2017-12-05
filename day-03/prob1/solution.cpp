7bc3e5f9c48988f8710b1e00a92ba1cb0498c17e
Magnus Gedda <github@gedda.se>
2017-12-05 09:51:30 +0100

    Cleanup

--- day-03/prob1/solution.cpp
+++ day-03/prob1/solution.cpp
@@ -3,33 +3,47 @@
 #include <string>
 #include <sstream>
 #include <climits>
+#include <assert.h>
 
 using namespace std;
 
-int main()
+int getDistanceTo(int square)
 {
-  int square;
-
-  cin >> square;
+  int distance = 0;
 
   int i = 1;
   while (i * i < square) {
     i = i + 2;
   }
 
-  if (i == 1) {
-    cout << 0 << endl;
-  }
-  else {
+  if (i > 1) {
     int start_index = (i - 2) * (i - 2) + 1;
     int border_length = 4 * (i - 1);
     int border_index = square - start_index;
-
     int distance_sideways = abs(border_index % (i - 1) - (i/2 - 1));
-    int distance = /*distance_inwards*/ i / 2 + distance_sideways;  
-    
-    cout << distance << endl;
+    distance = /*distance_inwards*/ i / 2 + distance_sideways;    
   }
+
+  return distance;
+}
+
+
+int main()
+{
+  //-- Tests -----------
+  {
+    assert(getDistanceTo(1) == 0);
+    assert(getDistanceTo(12) == 3);
+    assert(getDistanceTo(23) == 2);
+    assert(getDistanceTo(1024) == 31);
+  }
+  //--------------------
+
+  int square;
+  cin >> square;
+
+  int distance = getDistanceTo(square);
+  cout << distance << endl;
 
   return 0;
 }
